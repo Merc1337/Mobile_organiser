143dcac6963ea3c5c95ecebee3b29335dfad2ba4
Merc1337 <51966929+Merc1337@users.noreply.github.com>
2022-04-17 17:06:37 +0300

    App.js
    Прикрепляю файлы с кодом.

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import Constants from 'expo-constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+import MainPage from './components/MainPage';
+import TaskPage from './components/Task';
+
+export default function App() {
+
+
+  return (
+ <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainPage">
+        <Stack.Screen 
+          name="MainPage"
+          component={MainPage}
+          options={{
+            title: 'Запланированные дела' //Set Header Title
+          }}
+        />
+        <Stack.Screen
+          name="Task"
+          component={TaskPage}
+          options={{
+            title: 'Task', //Set Header Title
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
