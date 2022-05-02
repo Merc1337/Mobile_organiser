bca3f3d007945f2b6ee75bf34bb7b3df3579bd90
Merc1337 <51966929+Merc1337@users.noreply.github.com>
2022-05-02 15:54:19 +0300

    Create MainPage.js

---
+++ components/MainPage.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity, StatusBar,ImageBackground } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather, Ionicons, FontAwesome5 } from '@expo/vector-icons'; 
+
+
+
+export default function App({navigation}) {
+  return(
+  <View style={styles.main}>
+  <ImageBackground source={require('../back.jpg')} resizeMode='cover' style={{width: '100%', height: '100%'}}>
+  <TouchableOpacity
+  style={styles.button}
+  onPress={() => navigation.navigate('Task')} >
+
+<Text style={styles.paragraph}><Ionicons name="md-shield-checkmark-sharp" size={24} color="white" /> Задачи</Text>
+</TouchableOpacity>
+  <TouchableOpacity
+  style={styles.button}
+  onPress={() => navigation.navigate('TaskCalendar')} >
+
+<Text style={styles.paragraph}><FontAwesome5 name="calendar-day" size={24} color="white"/> Календарь</Text>
+</TouchableOpacity>
+ <TouchableOpacity
+  style={styles.button}
+  onPress={() => navigation.navigate('About')}>
+<Text style={styles.paragraph}><Ionicons name="person" size={24} color="white" /> О приложении</Text>
+</TouchableOpacity>
+</ImageBackground>
+
+  </View>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  paragraph: {
+    fontSize:26,
+    fontWeight: 'regular',
+    color: 'white'
+
+
+  },
+   button: {
+    alignItems: "center",
+    backgroundColor: "#954AA2", 
+    padding: 10,
+    marginTop: '3%'
+  },
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    
+  },
+
+});
