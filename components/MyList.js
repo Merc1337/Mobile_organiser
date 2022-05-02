0e32d38c5cdffc8721c047d1984d97f7057dde91
Merc1337 <51966929+Merc1337@users.noreply.github.com>
2022-05-02 15:53:45 +0300

    Create MyList.js

---
+++ components/MyList.js
@@ -0,0 +1,41 @@
+
+import React, {useState} from 'react';
+import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity, ImageBackground } from 'react-native';
+import Constants from 'expo-constants';
+
+
+
+
+
+export default function MyList({el, delHandler}) {
+
+  return (
+    <View >
+
+    <TouchableOpacity   onLongPress={ () => delHandler(el.key)}>
+      <Text style={styles.text}>{el.text}</Text>
+    </TouchableOpacity>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    padding: 20,
+    textAlign: 'center',
+    borderRadius: 10,
+    backgroundColor: '#954AA2',
+    borderWidth: 1,
+    marginTop: 20,
+    width: '60%',
+    marginLeft: '20%',
+    color: 'white',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
